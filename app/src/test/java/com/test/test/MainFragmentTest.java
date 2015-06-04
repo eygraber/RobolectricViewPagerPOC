70eafaef15ccb3ddac0564558e29a5b9fe9b948d
Eli Graber <eli@isapp.com>
2015-06-03 22:26:42 -0400

    Made changes to get up to date

--- app/src/test/java/com/test/test/MainFragmentTest.java
+++ app/src/test/java/com/test/test/MainFragmentTest.java
@@ -1,12 +1,13 @@
 package com.test.test;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
-@RunWith(RobolectricTestRunner.class)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class MainFragmentTest {
   private MainFragment subject;
 
